b91bf4bc5a16133a49adddfe4981510fa36a6f12
ariptom <95114854+ariptom@users.noreply.github.com>
2023-03-07 22:06:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,6 @@ function preload () {}
 function setup () {
     canvas=createCanvas(400,300);
     canvas.center();
-    video=createCapture(VIDEO);
-    video.hide();
-    posenet=ml5.poseNet(video,modelReady);
-    posenet.on("pose",gotResults);
-}
-function modelReady(){
-    console.log("INITIALIZED!!");
-}
-function gotResults(results){
-    if (results.length>0){
-        console.log(results);
-        nosex=results[0].pose.nose.x;
-        nosey=results[0].pose.nose.y;
-    }
-}
 function draw(){
     image(video,0,0,400,300);
 }
