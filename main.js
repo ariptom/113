a0ac621f198ccdae60d58342cb831a2c8f46ff57
ariptom <95114854+ariptom@users.noreply.github.com>
2023-03-07 22:07:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ function preload () {}
 function setup () {
     canvas=createCanvas(400,300);
     canvas.center();
-function draw(){
-    image(video,0,0,400,300);
-}
+function draw(){}
 function take_snapshot (){
     save('myimage.png');
 }
